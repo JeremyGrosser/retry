e7fc60f02e6287286d4f4cdc6cd6d1f4c5ec22cc
Jeremy Grosser <jeremy@synack.me>
2020-07-27 18:38:27 -0700

    add missing header for waitpid macros

--- retry.c
+++ retry.c
@@ -16,6 +16,7 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
+#include <sys/wait.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
