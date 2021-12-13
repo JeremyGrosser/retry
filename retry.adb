b0ddca391ac7a458690e8c47ec4da250af80a956
Jeremy Grosser <jeremy@synack.me>
2021-12-13 16:09:05 +0000

    Fix off by one error when using nonzero success codes

--- retry.adb
+++ retry.adb
@@ -6,12 +6,12 @@
 --  modify it under the terms of the GNU General Public License
 --  as published by the Free Software Foundation; either version 2
 --  of the License, or (at your option) any later version.
---  
+--
 --  This program is distributed in the hope that it will be useful,
 --  but WITHOUT ANY WARRANTY; without even the implied warranty of
 --  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 --  GNU General Public License for more details.
---  
+--
 --  You should have received a copy of the GNU General Public License
 --  along with this program; if not, write to the
 --  Free Software Foundation, Inc.
@@ -91,6 +91,10 @@ procedure Retry is
                     return;
             end case;
         end loop;
+
+        if SC_Index /= Success_Codes'First then
+           SC_Index := SC_Index - 1;
+        end if;
     end Parse_Options;
 begin
     Seconds_IO.Default_Fore := 0;
